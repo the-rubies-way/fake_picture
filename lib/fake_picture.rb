838b5e108920c0252f507e4478399f290d86ea86
loqimean <vanuha277@gmail.com>
2022-03-02 19:42:11 +0200

    initialize commit

---
+++ lib/fake_picture.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fake_picture/version"
+
+module FakePicture
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/fake_picture_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe FakePicture do
+  it "has a version number" do
+    expect(FakePicture::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
