3092c2bfa01e2f70fb6632ae5c678d8eb425ed77
loqimean <vanuha277@gmail.com>
2022-04-02 14:10:27 +0300

    remove superfluous code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,13 @@
 require "fake_picture"
 require 'debug'
 require 'pathname'
+require 'simplecov'
+
+SimpleCov.start
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  # config.example_status_persistence_file_path = ".rspec_status"
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
